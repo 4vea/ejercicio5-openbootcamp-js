19d9564fca2b530ab76c6b32734646e71fa4144e
Le4anndr0 <ricardosk8532@gmail.com>
2023-05-13 19:11:17 -0300

    reentregando proyectos

--- ejercicio5.js
+++ ejercicio5.js
@@ -1,6 +1,11 @@
 const alturaEnCentimetros = 170;
+
 const alturaEnMetros = 1.70;
+
 const pesoEnKilogramos = 70.40;
-const alturaEnMetrosRedondeadaHaciaAbajo = Math.round((alturaEnMetros)*100)/100;
-const pesoEnKilogramosRedondeadaHaciaAbajo = Math.round((pesoEnKilogramos)*100)/100;
+
+const alturaEnMetrosRedondeadaHaciaAbajo = Math.ceil(alturaEnMetros)
+
+const pesoEnKilogramosRedondeadaHaciaAbajo = Math.floor(pesoEnKilogramos)
+
 const variableMaximos = Number.MAX_VALUE + 1 === Number.MAX_VALUE;
